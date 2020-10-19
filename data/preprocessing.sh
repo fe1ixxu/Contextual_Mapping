37797eb6e5fcf2b964f5f7624ecb3d4a1e90a9ee
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2020-10-18 20:36:38 -0400

    Update preprocessing.sh

--- data/preprocessing.sh
+++ data/preprocessing.sh
@@ -2,7 +2,8 @@
 
 src=$en
 tgt=$1
-path=YOUR/PATH/FOR/PARALLEL/CORPUS # point our your path of folder for downloaded corpora 
+path=$2 #YOUR/PATH/FOR/PARALLEL/CORPUS, point our your path of folder for downloaded corpora 
+fastalign=$3
 
 mkdir ${path}vectors/
 mkdir ${path}aligned/
@@ -10,7 +11,7 @@ mkdir ${path}aligned/
 python create_sen.py --file $path$src-$tgt.txt --en_file $path$src.txt --lg_file $path$tgt.txt
 python bert_token_sen.py --src $src --tgt $tgt --src_file $path${src}.txt --tgt_file $path${tgt}.txt --write_src_file $path${src}_token.txt --write_tgt_file $path${tgt}_token.txt
 python para-src-tgt.py --src_file $path${src}_token.txt --tgt_file $path${tgt}_token.txt --write_file ${path}para_${src}_${tgt}.txt
-PATH/FOR/FAST/ALIGN/fast_align -i ${path}para_${src}_${tgt}.txt -d -o -v >${path}forward_align.txt # point out your path for fast align
+${fastalign}/fast_align -i ${path}para_${src}_${tgt}.txt -d -o -v >${path}forward_align.txt # point out your path for fast align
 
 rm ${path}${src}.txt
 rm ${path}${tgt}.txt
